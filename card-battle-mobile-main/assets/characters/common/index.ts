0ad32f4d0e5a52c30c0cdbe7f1cfa5d1295c87e8
3thelast-glitch <3thelast@gmail.com>
2026-03-23 10:17:32 +0300

    feat: Create common character image index with paths to common character images.

--- card-battle-mobile-main/assets/characters/common/index.ts
+++ card-battle-mobile-main/assets/characters/common/index.ts
@@ -1,14 +1,13 @@
 export const COMMON_IMAGES: Record<string, any> = {
     yosaku: require('./yosaku.jpg'),
-    johnny: require('./johnny.jpg'),
     nami: require('./nami.jpg'),
     ebisu: require('./ebisu.jpg'),
     konohamaru: require('./konohamaru.jpg'),
     bulma: require('./bulma.jpg'),
     hitch_dreyse: require('./hitch_dreyse.jpg'),
-    misa_amane: require('./misa_amane.jpg'),
     king: require('./king.jpg'),
     happy: require('./happy.jpg'),
     mineta: require('./mineta.jpg'),
-    the_plain_doll: require('./the_plain_doll.png'),
+    the_plain_doll: require('./the_plain_doll.jpg'),
+    leorio: require('./leorio.jpg'),
 };
